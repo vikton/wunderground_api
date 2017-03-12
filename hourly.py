3f3ddef5afcc7e35dfa6eb950f8b592e6a23b2c4
Victor Ferrer <vikton@aules-114-249.udl.net>
2017-03-12 12:04:31 +0100

    Finished

--- hourly.py
+++ hourly.py
@@ -13,7 +13,9 @@ import urllib2
 from bs4 import BeautifulSoup
 
 api_key = None
-location = None
+location = ""
+number = None
+
 
 class WheatherHourly(object):
     """Wheather Client."""
@@ -32,30 +34,31 @@ class WheatherHourly(object):
         """Read and dowload the web."""
         url = WheatherHourly.url_base + self.api_key + \
             WheatherHourly.url_service["hourly"] + \
-            location + ".json"
-        return url
-
-
-class WheatherAlmanac(object):
-    """Docstring dor WheatherClient."""
-
-    url_base = "http://api.wunderground.com/api/"
-    url_service = {
-                   "almanac": "/almanac/q/CA/"
-                  }
-
-    def __init__(self, api_key):
-        """Init Class."""
-        super(WheatherAlmanac, self).__init__()
-        self.api_key = api_key
-
-    def almanac(self, location):
-        """Almanac dosctring."""
-        # baixar-se web
-        url = WheatherAlmanac.url_base + self.api_key + \
-            WheatherAlmanac.url_service["almanac"] + \
             location + ".xml"
-        return url
+
+        f = urllib2.urlopen(url)
+        html = f.read()
+        f.close()
+        soup = BeautifulSoup(html, 'lxml')
+        res = soup.find_all("forecast")
+        i = 0
+        for elements in res:
+            if int(i) >= int(number):
+                break
+            pretty = elements.find("pretty")
+            temp1 = elements.find("temp")
+            temp = temp1.find("metric")
+            condition = elements.find("condition")
+            feelslike1 = elements.find("feelslike")
+            feelslike = feelslike1.find("metric")
+            snow1 = elements.find("snow")
+            snow = snow1.find("metric")
+            humidity = elements.find("humidity")
+            print "---->Prediction the " + pretty.text + ":"
+            print "The temperature will be around " + temp.text + " C, with a feelike of " + feelslike.text + " C. For this reason the condition of the sky will be " + condition.text + "."
+            print "The humidity will not exceed " + humidity.text + " and the probability of snowing at " + snow.text + "."
+            print "--------------------------"
+            i = i + 1
 
 
 if __name__ == "__main__":
@@ -65,8 +68,10 @@ if __name__ == "__main__":
         except IndexError:
             print "You have to introduce your API wunderground key"
             exit(-1)
-wa = WheatherAlmanac(api_key)
+
+
+print("Introduce the number of hours from now you would like to consult:")
+number = input()
+print "---You asked for " + str(number) + " hourly weather in Lleida---"
 wh = WheatherHourly(api_key)
-resultatwa = wa.almanac("Lleida")
 resultatwh = wh.hourly("Lleida")
-# print resultat
